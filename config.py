cc81bcb497488de0b59600b9aac49e3029d9b59a
coconut8536 <67361664+coconut8536@users.noreply.github.com>
2025-08-11 14:12:02 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py — настройки бота
+
+TOKEN = "" # Вставте свой токен бота @tokenfather
+CHAT_ID = ""  # Узнать через @userinfobot
+DATA_FILE = "battery_bot.json"  # Файл для сохранения данных
+CHECK_INTERVAL = 180  # Интервал проверки (сек)
+MIN_CHANGE_PERCENT = 5  # Минимальное изменение заряда для обновления
+EDIT_COOLDOWN = 30  # Минимальное время между редактированием (сек)
+CRITICAL_LEVEL = 15  # Критический заряд (%)
+CRITICAL_ALERT_LIFETIME = 300  # Сколько секунд живёт экстренное сообщение
